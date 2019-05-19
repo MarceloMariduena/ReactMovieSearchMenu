f7df99cd3eb408024cfcd48385568f239a5b4378
unknown <marcelomariduena@protonmail.com>
2019-05-19 14:38:02 -0400

    Updates

---
+++ precache-manifest.3219b5f3b8f3184dc426215ded2e4239.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98500b339ae6de60ace16cc133089d36",
+    "url": "/ReactMovieSearchMenu/index.html"
+  },
+  {
+    "revision": "698decdd0d4fde3919b7",
+    "url": "/ReactMovieSearchMenu/static/css/main.26bcb91e.chunk.css"
+  },
+  {
+    "revision": "412245beafdaf8380fa7",
+    "url": "/ReactMovieSearchMenu/static/js/2.47c941ab.chunk.js"
+  },
+  {
+    "revision": "698decdd0d4fde3919b7",
+    "url": "/ReactMovieSearchMenu/static/js/main.6fe48690.chunk.js"
+  },
+  {
+    "revision": "3bad9e13a08af371a781",
+    "url": "/ReactMovieSearchMenu/static/js/runtime~main.c77194c3.js"
+  },
+  {
+    "revision": "2e8971470bb85902e96c8eddbb9a5254",
+    "url": "/ReactMovieSearchMenu/static/media/darkKnight.2e897147.jpg"
+  },
+  {
+    "revision": "d88b828bc5e7d9a13b7a492eb4135973",
+    "url": "/ReactMovieSearchMenu/static/media/darkKnightRises.d88b828b.jpeg"
+  },
+  {
+    "revision": "b3defc07dfaa1b6f5b74852ce38a3f8f",
+    "url": "/ReactMovieSearchMenu/static/media/fightClub.b3defc07.jpg"
+  },
+  {
+    "revision": "e0cadb77585ec473117d6f8350670c30",
+    "url": "/ReactMovieSearchMenu/static/media/goneGirl.e0cadb77.jpg"
+  },
+  {
+    "revision": "236f2f0cf1927f5d43b2d0224088bfce",
+    "url": "/ReactMovieSearchMenu/static/media/infinityWar.236f2f0c.jpg"
+  },
+  {
+    "revision": "0eb6e9c66ce234f0aac0c9d87b46f853",
+    "url": "/ReactMovieSearchMenu/static/media/nightcrawler.0eb6e9c6.jpg"
+  },
+  {
+    "revision": "bd2b6102aac2d056fa889d3caa0609f9",
+    "url": "/ReactMovieSearchMenu/static/media/theHouseThatJackBuilt.bd2b6102.jpg"
+  },
+  {
+    "revision": "f3343a7b8f9d7a5d6a92673047d4a950",
+    "url": "/ReactMovieSearchMenu/static/media/unbreakable.f3343a7b.jpg"
+  }
+]);
